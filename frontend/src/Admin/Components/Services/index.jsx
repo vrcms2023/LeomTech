0a784a7a27d83bddf723db39eb356dbafc196b62
Ramakrishna <jetti@Ramakrishna.local>
2023-12-17 20:51:58 +0530

    update service

--- frontend/src/Admin/Components/Services/index.jsx
+++ frontend/src/Admin/Components/Services/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { toast } from "react-toastify";
 import Button from "../../../Common/Button";
-import { Link } from "react-router-dom";
+import {Link} from 'react-router-dom'
 import {
   axiosClientServiceApi,
   axiosServiceApi,
@@ -12,13 +12,13 @@ import { confirmAlert } from "react-confirm-alert";
 import DeleteDialog from "../../../Common/DeleteDialog";
 import Title from "../../../Common/Title";
 
-import "./services.css";
+import './services.css'
 
 const AddService = ({ setSelectedServiceProject, selectedServiceProject }) => {
   const [serviceName, setServiceName] = useState("");
   const [error, setError] = useState("");
   const [serviceList, setServiceList] = useState([]);
-  const [editServiceObject, setEditServiceObject] = useState("");
+  const [editServiceObject, setEditServiceObject] = useState('');
   const [userName, setUserName] = useState("");
   const onPageLoadAction = useRef(true);
 
@@ -27,9 +27,10 @@ const AddService = ({ setSelectedServiceProject, selectedServiceProject }) => {
     setServiceName(event.target.value);
   };
 
-  const onClickSelectedService = (item) => {
-    setSelectedServiceProject(item);
-  };
+
+  const onClickSelectedService = (item) =>{
+    setSelectedServiceProject(item)
+  }
 
   useEffect(() => {
     setUserName(getCookie("userName"));
@@ -43,21 +44,18 @@ const AddService = ({ setSelectedServiceProject, selectedServiceProject }) => {
       setError("Please Enter Service  name");
       return;
     }
-    let response = "";
-    let data = {
+    let response =""
+    let data ={
       services_page_title: serviceName,
       created_by: userName,
       publish: editServiceObject.publish ? true : false,
-    };
+    }
     try {
-      if (editServiceObject?.id) {
-        data["id"] = editServiceObject.id;
-        data["updated_by"] = userName;
-        response = await axiosServiceApi.put(
-          `/services/updateService/${editServiceObject.id}/`,
-          data,
-        );
-      } else {
+      if(editServiceObject?.id){
+        data['id'] = editServiceObject.id;
+        data['updated_by'] = userName
+        response = await axiosServiceApi.put(`/services/updateService/${editServiceObject.id}/`, data);
+      }else {
         response = await axiosServiceApi.post(`/services/createService/`, data);
       }
       if (response?.status == 201 || response?.status == 200) {
@@ -77,7 +75,7 @@ const AddService = ({ setSelectedServiceProject, selectedServiceProject }) => {
   const getServiceList = async () => {
     try {
       const response = await axiosServiceApi.get(`/services/createService/`);
-      console.log(response, "Services");
+      console.log(response, "Services")
       if (response?.status === 200) {
         setServiceList(response.data.services);
         if (onPageLoadAction.current) {
@@ -90,6 +88,7 @@ const AddService = ({ setSelectedServiceProject, selectedServiceProject }) => {
     }
   };
 
+
   /**
    *  get Service list on page load
    */
@@ -97,7 +96,10 @@ const AddService = ({ setSelectedServiceProject, selectedServiceProject }) => {
     getServiceList();
   }, []);
 
+
+
   const publishService = async (item) => {
+ 
     try {
       let response = await axiosServiceApi.patch(
         `/services/publishService/${item.id}/`,
@@ -107,7 +109,7 @@ const AddService = ({ setSelectedServiceProject, selectedServiceProject }) => {
       if (response.status === 200) {
         toast.success(`Service published successfully`);
         setSelectedServiceProject(response.data.services);
-        getServiceList();
+        getServiceList()
       }
     } catch (error) {
       console.log("unable to publish the services");
@@ -115,6 +117,7 @@ const AddService = ({ setSelectedServiceProject, selectedServiceProject }) => {
   };
 
   const deleteService = (item) => {
+
     const id = item.id;
     const name = item.services_page_title;
     const deleteImageByID = async () => {
@@ -142,24 +145,26 @@ const AddService = ({ setSelectedServiceProject, selectedServiceProject }) => {
   };
 
   const EditService = (item) => {
-    setServiceName(item.services_page_title);
-    setEditServiceObject(item);
-  };
+    setServiceName(item.services_page_title)
+    setEditServiceObject(item)
+  }
 
   return (
     <div className="my-5">
-      <h3 className={`text-center `}>Add New Service </h3>
+
+<h3 className={`text-center `}>Add New Service </h3>
 
       {/* <h3 className={`text-center ${selectedServiceProject && selectedServiceProject.publish ? 'border border-success' : ''} `}>Add New Service </h3> */}
 
       <div className={`container bg-light p-5 border shadow-lg`}>
-        {/* <div className={`container bg-light p-5 border shadow-lg ${selectedServiceProject && selectedServiceProject.publish ? 'border border-success' : ''}`}> */}
+      {/* <div className={`container bg-light p-5 border shadow-lg ${selectedServiceProject && selectedServiceProject.publish ? 'border border-success' : ''}`}> */}
         <div className="row">
           {error ? <Error>{error}</Error> : ""}
           <div className="col-md-7 text-center">
+          
             <input
               type="text"
-              cssClass="form-control py-2"
+              class="form-control py-2"
               name="services_page_title"
               id=""
               value={serviceName}
@@ -171,49 +176,30 @@ const AddService = ({ setSelectedServiceProject, selectedServiceProject }) => {
               type="submit"
               cssClass="btn btn-lg btn-primary mt-3"
               handlerChange={submitHandler}
-              label={editServiceObject?.id ? "Update Service Name" : "Save"}
+              label={editServiceObject?.id ? "Update Service Name" :"Save"}
             />
-          </div>
+            </div>
 
-          <div className="col-md-5 mt-5 mt-md-0 servicePageLinks">
-            <Title
-              title="Pages"
-              cssClass="fs-6 fw-bold text-center border-bottom pb-2 mb-2 "
-            />
+            <div className="col-md-5 mt-5 mt-md-0 servicePageLinks">
+              <Title title="Pages" cssClass="fs-6 fw-bold text-center border-bottom pb-2 mb-2 " />
             <ul>
-              {serviceList &&
-                serviceList.map((item, index) => (
-                  <li
-                    className={`d-flex justify-content-between p-1
-              ${selectedServiceProject ? "border border-success" : ""}`}
-                    key={item.id}
-                    onClick={(event) => onClickSelectedService(item)}
-                  >
-                    <div className="fw-bold ">{item.services_page_title} </div>
-                    <div>
-                      <Link
-                        onClick={publishService}
-                        className={`p-1 px-3 mx-2 rounded ${
-                          item.publish
-                            ? "bg-success text-white"
-                            : "bg-secondary text-light"
-                        }`}
-                      >
-                        <small>
-                          {item.publish ? "Published" : "Un Publish"}
-                        </small>
-                      </Link>
-                      <Link onClick={deleteService}>
-                        {" "}
-                        <i
-                          class="fa fa-trash-o text-danger fs-4"
-                          aria-hidden="true"
-                        ></i>
-                      </Link>
-                    </div>
-                  </li>
-                ))}
+            {serviceList && serviceList.map((item) => (
+              <li className={`d-flex justify-content-between p-1
+              ${item.id === selectedServiceProject?.id ? 'border border-success' : ''}`
+              } key={item.id} >
+
+                <Link onClick={(event) => onClickSelectedService(item)} className="fw-bold ">{item.services_page_title} </Link>
+                <div>
+                <Link onClick={()=>publishService(item)} className={`p-1 px-3 mx-2 rounded ${item.publish ? "bg-success text-white" : "bg-secondary text-light"}`}>
+                    <small>{item.publish ? "Published" : "Un Publish"}</small>
+                  </Link>
+                  <Link onClick={() =>EditService(item)}> <i class="fa fa-pencil text-danger fs-4" aria-hidden="true"></i></Link>
+                  <Link onClick={() =>deleteService(item)}> <i class="fa fa-trash-o text-danger fs-4" aria-hidden="true"></i></Link>
+                </div>
+              </li>
+              ))}
             </ul>
+            
           </div>
         </div>
       </div>
