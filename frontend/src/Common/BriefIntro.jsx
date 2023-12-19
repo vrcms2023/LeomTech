7e7062963de28bf0cb4a3f8be3746288835b6309
unknown <prasadnti@gmail.com>
2023-12-19 17:18:39 +0530

    Page Horizontal scroll fix

--- frontend/src/Common/BriefIntro.jsx
+++ frontend/src/Common/BriefIntro.jsx
@@ -28,37 +28,39 @@ const BriefIntroFrontend = ({ pageType, introState }) => {
   }, [introState]);
 
   return (
-    <div className="row">
-        <div className=" py-4 mx-0 briefIntro">
-          <div className="col-md-10 offset-md-1 px-4 py-2 py-md-4 ">
-            {introValue.intro_title === "" ? "" : 
-              <Title
-                title={`${
-                  introValue.intro_title
-                    ? introValue.intro_title
-                    : "Please Update Title"
-                }`}
-                cssClass="mb-0 fw-bold fs-2 text-center"
-              />
-            }
-            {introValue.subTitle === "" ? "" : 
-              <Title
-                title={`${
-                  introValue.intro_title
-                    ? introValue.subTitle
-                    : "Please Update Title"
-                }`}
-                cssClass="mb-3 fw-bold text-secondary fs-6 text-center"
-              />
-            }
-            <p className="text-center lh-md m-0 fs-6 fw-medium">
-              {introValue.intro_desc
-                ? introValue.intro_desc
-                : "Please Update Description"}
-            </p>
+    <div className="container-fluid">
+      <div className="row">
+          <div className=" py-4 mx-0 briefIntro">
+            <div className="col-md-10 offset-md-1 px-4 py-2 py-md-4 ">
+              {introValue.intro_title === "" ? "" : 
+                <Title
+                  title={`${
+                    introValue.intro_title
+                      ? introValue.intro_title
+                      : "Please Update Title"
+                  }`}
+                  cssClass="mb-0 fw-bold fs-2 text-center"
+                />
+              }
+              {introValue.subTitle === "" ? "" : 
+                <Title
+                  title={`${
+                    introValue.intro_title
+                      ? introValue.subTitle
+                      : "Please Update Title"
+                  }`}
+                  cssClass="mb-3 fw-bold text-secondary fs-6 text-center"
+                />
+              }
+              <p className="text-center lh-md m-0 fs-6 fw-medium">
+                {introValue.intro_desc
+                  ? introValue.intro_desc
+                  : "Please Update Description"}
+              </p>
+            </div>
           </div>
-        </div>
       </div>
+    </div>
   );
 };
 
